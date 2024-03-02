9503fa04e9fc44af385d7d37ce4226f8a6cf837f
Nemanja324 <160654863+Nemanja324@users.noreply.github.com>
2024-03-02 17:47:13 +0000

    varijanta bez if uslova

--- Domaci1 zadaci/zad32.py
+++ Domaci1 zadaci/zad32.py
@@ -1,15 +1,19 @@
 broj = input('Unesite sestocifren broj: ')
-if len(broj) == 6:
-    a = int(broj[0])
-    b = int(broj[1])
-    c = int(broj[2])
-    d = int(broj[3])
-    e = int(broj[4])
-    f = int(broj[5])
-    if a*c+2+f == b+d*e:
+# if len(broj) == 6:
+a = int(broj[0])
+b = int(broj[1])
+c = int(broj[2])
+d = int(broj[3])
+e = int(broj[4])
+f = int(broj[5])
+'''    if a*c+2+f == b+d*e:
         print(True)
     else:
-        print(False)
+        print(False)'''
 
-else:
-    print('Niste unijeli sestocifren broj, probajte ponovo.')
+'''else:
+    print('Niste unijeli sestocifren broj, probajte ponovo.')'''
+
+lijevo = a*c+2+f
+desno = b+d*e
+print(f'Rezultat poredjenja je {lijevo == desno}')
